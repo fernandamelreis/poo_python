7a4afdcae97a8878d62cf18b718376298b1647f3
Fernanda de Melo Reis <114718084+fernandamelreis@users.noreply.github.com>
2023-08-10 19:34:33 -0300

    Implementando funcionalidades no CRUD Produto
    Nesse commit foi feita impletamentação no código do produto com a criação das funcionalidades do Atualizar e Deletar.

--- produto.py
+++ produto.py
@@ -6,7 +6,7 @@ class Produto:
         self.tamanho = tamanho
         
     def Cadastrar_Produto(self):
-        print("--- CADASTRANDO PRODUTO ---- ")
+        print("--- CADASTRANDO PRODUTO ---")
         nome = input("Digite o nome do produto: ")
         desc = input("Digite a descrição do produto: ")
         preco = float(input("Digite o preço do produto: "))
@@ -26,6 +26,7 @@ class Produto:
             print("Fim de operação!")
     
     def Autenticar_Produto(self):
+        print("--- VERIFICANDO PRODUTO ---")
         nome = input("Digite o nome do produto: ")
         desc = input("Digite a descrição do produto: ")
         preco = float(input("Digite o preço do produto: "))
@@ -37,10 +38,85 @@ class Produto:
             print("Produto não encontrado no estoque!")
         
     def Atualizar_Produto(self):
-        print("Produto atualizado!")
+        print("Atualizando produto")
+        
+        print(self.nome)
+        print(self.desc)
+        print(self.preco)
+        print(self.tamanho)
+        
+        resp = input("Deseja atualizar o produto: [y ou n]")
+        
+        if (resp == 'y'):
+            
+            print("[1] NOME PRODUTO")
+            print("[2] DESCRIÇÃO PRODUTO")
+            print("[3] PREÇO PRODUTO")
+            print("[4] TAMANHO PRODUTO")
+            
+            opcao = int(input("Digite a opção que deseja alterar: "))
+            
+            if (opcao == 1):
+                print("Atualizando nome do produto")
+                self.nome = input("Digite o novo nome para o produto: ")
+                print("Nome do produto atualizado!")
+                print(self.nome)
+            elif (opcao == 2):
+                print("Atualizando descrição do produto")
+                self.desc = input("Digite a nova descrição para o produto: ")
+                print("Descrição do produto atualizada!")
+                print(self.desc)
+            elif (opcao == 3):
+                print("Atualizando preço do produto")
+                self.preco = float(input("Digite o novo preço para o produto: "))
+                print("Preço do produto atualizada!")
+                print(self.preco)
+            elif (opcao == 4):
+                print("Atualizando tamanho do produto")
+                self.tamanho = input("Digite o novo tamanho para o produto: ")
+                print("Tamanho do produto atualizada!")
+                print(self.tamanho)
+            else:
+                print("Digite uma opção válida!")
+        else:
+            print("Encerrando operação...")
+            
+        print("----------------------------------")
+        print("----- PRODUTO ATUALIZADO -----")
+        print("CONFIRA TODAS AS INFORMAÇÕES DO PRODUTO:")
+        print(self.nome)
+        print(self.desc)
+        print(self.preco)
+        print(self.tamanho)
         
     def Deletar_Produto(self):
-        print("Produto deletado!")
+        print("--- DELETAR PRODUTO ---")
+        print("INFORMAÇÕES DO PRODUTO:")
+        
+        print("Nome: {}".format(self.nome))
+        print("Descrição: {}".format(self.desc))
+        print("Preço: R$ {}".format(self.preco))
+        print("Tamanho: {}".format(self.tamanho))
+        
+        
+        resp = input("Deseja realmente excluir o produto no estoque: [sim] ou [não]")
+        
+        if (resp == 'sim'):
+            self.nome = None
+            self.desc = None
+            self.preco = None
+            self.tamanho = None
+            print("Produto excluído do estoque!")
+        else:
+            print("Fim de operação... Produto não excluído!")
+        
+        print("----------------------------------")
+        print("----- LISTAGEM DE PRODUTO -----")
+        print("CONFIRA TODAS AS INFORMAÇÕES DO PRODUTO:")
+        print(self.nome)
+        print(self.desc)
+        print(self.preco)
+        print(self.tamanho)
         
 p1 = Produto('Pincel', 'cor vermelho', 7.00, 'não especificado')
-p1.Autenticar_Produto()
+p1.Deletar_Produto()
